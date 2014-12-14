f260c1cb35a733649d3fe07cb931421ab9bed7bb
kanastasov <kiril_anastasov@yahoo.com>
2014-12-14 14:44:13 +0200

    8 Wait and Notify

--- 8_WaitAndNotify/src/waitNotify/App.java
+++ 8_WaitAndNotify/src/waitNotify/App.java
@@ -1,5 +1,21 @@
 package waitNotify;
 
+/** <p>
+ * 	Wait and notify in Java; low-level multithreading methods of the
+ *  Object class that allow you to have one or more threads sleeping,
+ *  only to be woken up by other threads at the right moment.
+ *  Extremely useful for avoiding those processor-consuming polling loops.
+ * </p>
+ * 
+ * <p>
+ * The full tutorial and the majority of the code is available at
+ * https://www.udemy.com/java-multithreading/?dtcode=KmfAU1g20Sjj#/
+ * </p>
+ * 
+ * <p>
+ * @author kanastasov L1087591@live.tees.ac.uk December-2014
+ * </p>
+ */
 public class App {
 
     public static void main(String[] args) throws InterruptedException {
